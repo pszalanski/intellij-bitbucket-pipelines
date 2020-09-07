1fb61812e755b3a9aa971b2bd219b83700682103
GitHub Action <action@github.com>
2020-09-07 09:28:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pszalanski/intellijbitbucketpipelines/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pszalanski.intellijbitbucketpipelines.services
+
+import com.github.pszalanski.intellijbitbucketpipelines.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
